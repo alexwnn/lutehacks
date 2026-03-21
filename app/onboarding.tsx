8366a60679e7c13c91a610c461900e7c14b7615a
ALEX <nguyena@plu.edu>
2026-03-21 15:41:13 -0700

    create base onboarding screen

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -1,9 +1,77 @@
-import { View, Text } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { router } from "expo-router";
+import { useState } from "react";
+import { Button, StyleSheet } from "react-native";
+import { View, Text, TextInput } from "react-native";
 
 export default function Onboarding() {
+    const [name, setName] = useState('');
+    const [height, setHeight] = useState('');
+    const [weight, setWeight] = useState('');
+    const [age, setAge] = useState('');
+
+    const handleSubmit = () => {
+        storeData(name, 'name');
+        storeData(height, 'height');
+        storeData(weight, 'weight');
+        storeData(age, 'age');
+        router.replace('/');
+    }
+    const storeData = async (value: string, key: string) => {
+        try {
+            await AsyncStorage.setItem(key, value);
+        } catch (error) {
+            console.error(error);
+        }
+    }
     return (
-        <View>
-            <Text>Onboarding</Text>
+        <View style={styles.container}>
+            <TextInput
+            style={styles.inputs}
+            onChangeText={setName}
+            placeholder="Enter Name"
+            value={name}
+            inputMode="text"
+            />
+            <TextInput
+            style={styles.inputs}
+            onChangeText={setHeight}
+            placeholder="Enter Height"
+            value={height}
+            inputMode="numeric"
+            />
+            <TextInput
+            style={styles.inputs}
+            onChangeText={setWeight}
+            placeholder="Enter Weight"
+            value={weight}
+            inputMode="numeric"
+            />
+            <TextInput
+            style={styles.inputs}
+            onChangeText={setAge}
+            placeholder="Enter Age"
+            value={age}
+            inputMode="numeric"
+            />
+            <Button title="Submit" onPress={handleSubmit} />
         </View>
     );
 }
+const styles = StyleSheet.create({
+    inputs: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: 1,
+        borderColor: 'gray',
+        padding: 10,
+        margin: 10,
+        borderRadius: 5,
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+});
