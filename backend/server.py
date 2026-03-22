dc296d2a0edb1976f41943e61f49159c2646607a
Max Lopez <familiacachorin@gmail.com>
2026-03-21 19:17:12 -0700

    call backend

--- backend/server.py
+++ backend/server.py
@@ -104,4 +104,4 @@ def get_places(lat, lng):
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5001)
--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,11 +1,15 @@
 import { useEffect, useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { Pedometer } from "expo-sensors";
+import * as Location from "expo-location";
 
 export default function Index() {
   const [isPedometerAvailable, setIsPedometerAvailable] = useState("checking");
   const [pastStepCount, setPastStepCount] = useState(0);
   const [currentStepCount, setCurrentStepCount] = useState(0);
+  const [location, setLocation] = useState<Location.LocationObject | null>(
+    null,
+  );
 
   const subscribe = async () => {
     const isAvailable = await Pedometer.isAvailableAsync();
@@ -26,6 +30,26 @@ export default function Index() {
       });
     }
   };
+  useEffect(() => {
+    async function getCurrentLocationAndGetPlaces() {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+
+      // ipconfig getifaddr en0
+      const response = await fetch(
+        `http://10.72.17.237:5001/get-places/lat/${location.coords.latitude}/lng/${location.coords.longitude}`,
+      );
+
+      console.log(await response.json());
+    }
+
+    getCurrentLocationAndGetPlaces();
+  }, []);
 
   useEffect(() => {
     const subscription = subscribe();
