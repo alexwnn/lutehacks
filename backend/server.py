fa21b4bfeb07bbfb75175129f49a97d6157fad4c
Max Lopez <familiacachorin@gmail.com>
2026-03-21 17:00:48 -0700

    setup api key

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,16 @@
 from flask import Flask
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv('GOOGLE_MAPS_API_KEY')
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+@app.route('/get-places/lat/<lat>/lng/<lng>', methods=['GET'])
+def get_places(lat, lng):
+    return f'Places near {lat}, {lng}'
 
 if __name__ == '__main__':
     app.run()
